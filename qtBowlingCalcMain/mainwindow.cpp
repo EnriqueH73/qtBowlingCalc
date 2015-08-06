e9be09a922a2f7d195b8c4a65752b0287bdb641c
Enrique Herrera <EnriqueH73@gmail.com>
2015-08-06 04:56:00 -0600

    set up a signal/slot connection between all the frames and the radio button pin map. It not working as yet.

--- qtBowlingCalcMain/mainwindow.cpp
+++ qtBowlingCalcMain/mainwindow.cpp
@@ -6,6 +6,28 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->bowlFrame_1,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_2,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_3,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_4,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_5,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_6,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_7,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_8,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_9,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_10,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball1,SLOT(setCurrentFrame(bowlFrame*)));
+
+    connect(ui->bowlFrame_1,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_2,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_3,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_4,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_5,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_6,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_7,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_8,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_9,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
+    connect(ui->bowlFrame_10,SIGNAL(mouseClick_OurFrameObj(bowlFrame*)),ui->bowlPinMap_ball2,SLOT(setCurrentFrame(bowlFrame*)));
 }
 
 MainWindow::~MainWindow()
